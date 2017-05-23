080c3cc62165aaa4335fca8d5b42ae2a6033ebdc
1125282366 <1125282366@qq.com>
2017-05-23 14:48:12 +0800

    add index.html

---
+++ js/main.js
@@ -0,0 +1,13 @@
+/**
+ * Created by kerui on 2016/10/26.
+ */
+
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        items:1,
+        loop:true,
+        autoplay:true,
+        autoplayTimeout:3000,
+        autoplayHoverPause:true
+    });
+});
